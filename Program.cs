0feef3a375318e6dd34af55d07e362554ee4e7c4
Danya <danielmaksimencko@yandex.ru>
2023-05-15 14:01:35 +0300

    Facade class

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using GitTestApp.Run;
+using System.Text.Json;
+
 namespace GitTestApp
 {
     internal static class Program
@@ -8,6 +11,12 @@ namespace GitTestApp
         [STAThread]
         static void Main()
         {
+            // отсюда удалить все нахер
+            //Workout w1 = new Workout(new DateTime(2023, 4, 1), 30.45m, 4.1m, 20, 9, 31, 86);
+            //string jsonString = JsonSerializer.Serialize<Workout>(w1);
+            //Workout? w2 = JsonSerializer.Deserialize<Workout>(jsonString);
+
+
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
---
+++ Run/WorkoutFacade.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitTestApp.Run
+{
+    internal static class WorkoutFacade
+    {
+        static WorkoutFacade()
+        {
+            Workouts = new List<Workout> { new Workout() };
+        }
+
+        public static List<Workout> Workouts;
+
+        public static decimal TotalDistance { get; private set; }
+        public static decimal TotalDuration { get; private set; }
+        public static decimal AvgDuration { get; private set; }
+
+        public static uint AvgSpeed { get; private set; }
+        public static uint MinSpeed { get; private set; }
+        public static uint MaxSpeed { get; private set; }
+        public static uint AvgPulse { get; private set; }
+
+        public static void UploadInfo(string filePath)
+        {
+            if (!File.Exists(filePath)) throw new FileNotFoundException("not found");
+
+
+
+        }
+    }
+}
