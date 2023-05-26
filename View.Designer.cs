20584a74f0dfd45c4498becd717c47e3d2027ec9
Unhuman95 <132668342+Unhuman95@users.noreply.github.com>
2023-05-26 19:29:55 +0300

    Новая вкладка

--- View.Designer.cs
+++ View.Designer.cs
@@ -30,6 +30,13 @@
         {
             tabControl1 = new TabControl();
             tabPageRun = new TabPage();
+            labelDist = new Label();
+            labelTotDur = new Label();
+            labelAvDur = new Label();
+            labelAvSpeed = new Label();
+            labelMiSpeed = new Label();
+            labelMaSpeed = new Label();
+            labelAvPulse = new Label();
             textBoxDist = new TextBox();
             textBoxAvDur = new TextBox();
             textBoxDur = new TextBox();
@@ -52,13 +59,7 @@
             textBox1 = new TextBox();
             button1 = new Button();
             tabPageHouses = new TabPage();
-            labelAvPulse = new Label();
-            labelMaSpeed = new Label();
-            labelMiSpeed = new Label();
-            labelAvSpeed = new Label();
-            labelAvDur = new Label();
-            labelTotDur = new Label();
-            labelDist = new Label();
+            tabPageCrime = new TabPage();
             tabControl1.SuspendLayout();
             tabPageRun.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBoxMin).BeginInit();
@@ -71,10 +72,12 @@
             tabControl1.Controls.Add(tabPageRun);
             tabControl1.Controls.Add(tabPageTemp);
             tabControl1.Controls.Add(tabPageHouses);
-            tabControl1.Location = new Point(11, 12);
+            tabControl1.Controls.Add(tabPageCrime);
+            tabControl1.Location = new Point(10, 9);
+            tabControl1.Margin = new Padding(3, 2, 3, 2);
             tabControl1.Name = "tabControl1";
             tabControl1.SelectedIndex = 0;
-            tabControl1.Size = new Size(885, 442);
+            tabControl1.Size = new Size(774, 332);
             tabControl1.TabIndex = 0;
             // 
             // tabPageRun
@@ -98,97 +101,166 @@
             tabPageRun.Controls.Add(pictureBoxMin);
             tabPageRun.Controls.Add(buttonLoad);
             tabPageRun.Controls.Add(pictureBoxPulse);
-            tabPageRun.Location = new Point(4, 29);
+            tabPageRun.Location = new Point(4, 24);
+            tabPageRun.Margin = new Padding(3, 2, 3, 2);
             tabPageRun.Name = "tabPageRun";
-            tabPageRun.Padding = new Padding(3);
-            tabPageRun.Size = new Size(877, 409);
+            tabPageRun.Padding = new Padding(3, 2, 3, 2);
+            tabPageRun.Size = new Size(766, 304);
             tabPageRun.TabIndex = 0;
             tabPageRun.Text = "Пробежки";
             tabPageRun.UseVisualStyleBackColor = true;
             // 
+            // labelDist
+            // 
+            labelDist.AutoSize = true;
+            labelDist.Location = new Point(589, 244);
+            labelDist.Name = "labelDist";
+            labelDist.Size = new Size(89, 15);
+            labelDist.TabIndex = 6;
+            labelDist.Text = "Пройдено (км)";
+            // 
+            // labelTotDur
+            // 
+            labelTotDur.AutoSize = true;
+            labelTotDur.Location = new Point(460, 213);
+            labelTotDur.Name = "labelTotDur";
+            labelTotDur.Size = new Size(127, 15);
+            labelTotDur.TabIndex = 6;
+            labelTotDur.Text = "Общая продолжит-ть";
+            // 
+            // labelAvDur
+            // 
+            labelAvDur.AutoSize = true;
+            labelAvDur.Location = new Point(589, 178);
+            labelAvDur.Name = "labelAvDur";
+            labelAvDur.Size = new Size(134, 15);
+            labelAvDur.TabIndex = 6;
+            labelAvDur.Text = "Средняя продолжит-ть";
+            // 
+            // labelAvSpeed
+            // 
+            labelAvSpeed.AutoSize = true;
+            labelAvSpeed.Location = new Point(645, 75);
+            labelAvSpeed.Name = "labelAvSpeed";
+            labelAvSpeed.Size = new Size(106, 15);
+            labelAvSpeed.TabIndex = 6;
+            labelAvSpeed.Text = "Средняя скорость";
+            // 
+            // labelMiSpeed
+            // 
+            labelMiSpeed.AutoSize = true;
+            labelMiSpeed.Location = new Point(426, 75);
+            labelMiSpeed.Name = "labelMiSpeed";
+            labelMiSpeed.Size = new Size(139, 15);
+            labelMiSpeed.TabIndex = 6;
+            labelMiSpeed.Text = "Минимальная скорость";
+            // 
+            // labelMaSpeed
+            // 
+            labelMaSpeed.AutoSize = true;
+            labelMaSpeed.Location = new Point(606, 9);
+            labelMaSpeed.Name = "labelMaSpeed";
+            labelMaSpeed.Size = new Size(143, 15);
+            labelMaSpeed.TabIndex = 6;
+            labelMaSpeed.Text = "Максимальная скорость";
+            // 
+            // labelAvPulse
+            // 
+            labelAvPulse.AutoSize = true;
+            labelAvPulse.Location = new Point(426, 9);
+            labelAvPulse.Name = "labelAvPulse";
+            labelAvPulse.Size = new Size(90, 15);
+            labelAvPulse.TabIndex = 6;
+            labelAvPulse.Text = "Средний пульс";
+            // 
             // textBoxDist
             // 
-            textBoxDist.Location = new Point(487, 326);
+            textBoxDist.Location = new Point(426, 244);
+            textBoxDist.Margin = new Padding(3, 2, 3, 2);
             textBoxDist.Name = "textBoxDist";
-            textBoxDist.Size = new Size(180, 27);
+            textBoxDist.Size = new Size(158, 23);
             textBoxDist.TabIndex = 5;
             // 
             // textBoxAvDur
             // 
-            textBoxAvDur.Location = new Point(487, 238);
+            textBoxAvDur.Location = new Point(426, 178);
+            textBoxAvDur.Margin = new Padding(3, 2, 3, 2);
             textBoxAvDur.Name = "textBoxAvDur";
-            textBoxAvDur.Size = new Size(180, 27);
+            textBoxAvDur.Size = new Size(158, 23);
             textBoxAvDur.TabIndex = 5;
             // 
             // textBoxDur
             // 
-            textBoxDur.Location = new Point(691, 284);
+            textBoxDur.Location = new Point(605, 213);
+            textBoxDur.Margin = new Padding(3, 2, 3, 2);
             textBoxDur.Name = "textBoxDur";
-            textBoxDur.Size = new Size(180, 27);
+            textBoxDur.Size = new Size(158, 23);
             textBoxDur.TabIndex = 5;
             // 
             // textBoxPulse
             // 
-            textBoxPulse.Location = new Point(487, 43);
+            textBoxPulse.Location = new Point(426, 32);
+            textBoxPulse.Margin = new Padding(3, 2, 3, 2);
             textBoxPulse.Name = "textBoxPulse";
-            textBoxPulse.Size = new Size(180, 27);
+            textBoxPulse.Size = new Size(158, 23);
             textBoxPulse.TabIndex = 5;
             // 
             // textBoxAvSpeed
             // 
-            textBoxAvSpeed.Location = new Point(691, 132);
+            textBoxAvSpeed.Location = new Point(605, 99);
+            textBoxAvSpeed.Margin = new Padding(3, 2, 3, 2);
             textBoxAvSpeed.Name = "textBoxAvSpeed";
-            textBoxAvSpeed.Size = new Size(180, 27);
+            textBoxAvSpeed.Size = new Size(158, 23);
             textBoxAvSpeed.TabIndex = 5;
             // 
             // textBoxMaSpeed
             // 
-            textBoxMaSpeed.Location = new Point(691, 43);
+            textBoxMaSpeed.Location = new Point(605, 32);
+            textBoxMaSpeed.Margin = new Padding(3, 2, 3, 2);
             textBoxMaSpeed.Name = "textBoxMaSpeed";
-            textBoxMaSpeed.Size = new Size(180, 27);
+            textBoxMaSpeed.Size = new Size(158, 23);
             textBoxMaSpeed.TabIndex = 5;
             // 
             // textBoxMiSpeed
             // 
-            textBoxMiSpeed.Location = new Point(487, 132);
+            textBoxMiSpeed.Location = new Point(426, 99);
+            textBoxMiSpeed.Margin = new Padding(3, 2, 3, 2);
             textBoxMiSpeed.Name = "textBoxMiSpeed";
-            textBoxMiSpeed.Size = new Size(180, 27);
+            textBoxMiSpeed.Size = new Size(158, 23);
             textBoxMiSpeed.TabIndex = 5;
             // 
             // labelMin
             // 
             labelMin.AutoSize = true;
-            labelMin.Location = new Point(7, 163);
+            labelMin.Location = new Point(6, 122);
             labelMin.Name = "labelMin";
-            labelMin.Size = new Size(421, 20);
+            labelMin.Size = new Size(330, 15);
             labelMin.TabIndex = 4;
             labelMin.Text = "Изменения длительности тренировок за последний месяц";
             // 
             // labelPulse
             // 
             labelPulse.AutoSize = true;
-            labelPulse.Location = new Point(7, 12);
+            labelPulse.Location = new Point(6, 9);
             labelPulse.Name = "labelPulse";
-            labelPulse.Size = new Size(415, 20);
+            labelPulse.Size = new Size(323, 15);
             labelPulse.TabIndex = 3;
             labelPulse.Text = "Изменения пульса во время занятий за последний месяц";
             // 
             // pictureBoxMin
             // 
             pictureBoxMin.BorderStyle = BorderStyle.Fixed3D;
-            pictureBoxMin.Location = new Point(7, 187);
-            pictureBoxMin.Margin = new Padding(3, 4, 3, 4);
+            pictureBoxMin.Location = new Point(6, 140);
             pictureBoxMin.Name = "pictureBoxMin";
-            pictureBoxMin.Size = new Size(474, 166);
+            pictureBoxMin.Size = new Size(415, 126);
             pictureBoxMin.TabIndex = 2;
             pictureBoxMin.TabStop = false;
             // 
             // buttonLoad
             // 
-            buttonLoad.Location = new Point(7, 361);
-            buttonLoad.Margin = new Padding(3, 4, 3, 4);
+            buttonLoad.Location = new Point(6, 271);
             buttonLoad.Name = "buttonLoad";
-            buttonLoad.Size = new Size(864, 41);
+            buttonLoad.Size = new Size(756, 31);
             buttonLoad.TabIndex = 1;
             buttonLoad.Text = "Загрузить данные";
             buttonLoad.UseVisualStyleBackColor = true;
@@ -197,10 +269,9 @@
             // pictureBoxPulse
             // 
             pictureBoxPulse.BorderStyle = BorderStyle.Fixed3D;
-            pictureBoxPulse.Location = new Point(7, 43);
-            pictureBoxPulse.Margin = new Padding(3, 4, 3, 4);
+            pictureBoxPulse.Location = new Point(6, 32);
             pictureBoxPulse.Name = "pictureBoxPulse";
-            pictureBoxPulse.Size = new Size(474, 116);
+            pictureBoxPulse.Size = new Size(415, 88);
             pictureBoxPulse.TabIndex = 0;
             pictureBoxPulse.TabStop = false;
             // 
@@ -214,10 +285,11 @@
             tabPageTemp.Controls.Add(textBox2);
             tabPageTemp.Controls.Add(textBox1);
             tabPageTemp.Controls.Add(button1);
-            tabPageTemp.Location = new Point(4, 29);
+            tabPageTemp.Location = new Point(4, 24);
+            tabPageTemp.Margin = new Padding(3, 2, 3, 2);
             tabPageTemp.Name = "tabPageTemp";
-            tabPageTemp.Padding = new Padding(3);
-            tabPageTemp.Size = new Size(877, 409);
+            tabPageTemp.Padding = new Padding(3, 2, 3, 2);
+            tabPageTemp.Size = new Size(766, 304);
             tabPageTemp.TabIndex = 1;
             tabPageTemp.Text = "Температура";
             tabPageTemp.UseVisualStyleBackColor = true;
@@ -225,8 +297,7 @@
             // 
             // monthCalendar1
             // 
-            monthCalendar1.Location = new Point(47, 49);
-            monthCalendar1.Margin = new Padding(10, 12, 10, 12);
+            monthCalendar1.Location = new Point(41, 37);
             monthCalendar1.Name = "monthCalendar1";
             monthCalendar1.TabIndex = 10;
             monthCalendar1.DateChanged += monthCalendarDateChanged;
@@ -234,60 +305,56 @@
             // label3
             // 
             label3.AutoSize = true;
-            label3.Location = new Point(329, 205);
+            label3.Location = new Point(288, 154);
             label3.Name = "label3";
-            label3.Size = new Size(109, 20);
+            label3.Size = new Size(86, 15);
             label3.TabIndex = 8;
             label3.Text = "Минимальная";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(345, 140);
+            label2.Location = new Point(302, 105);
             label2.Name = "label2";
-            label2.Size = new Size(68, 20);
+            label2.Size = new Size(53, 15);
             label2.TabIndex = 7;
             label2.Text = "Средняя";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(329, 72);
+            label1.Location = new Point(288, 54);
             label1.Name = "label1";
-            label1.Size = new Size(113, 20);
+            label1.Size = new Size(90, 15);
             label1.TabIndex = 6;
             label1.Text = "Максимальная";
             // 
             // textBox3
             // 
-            textBox3.Location = new Point(456, 201);
-            textBox3.Margin = new Padding(3, 4, 3, 4);
+            textBox3.Location = new Point(399, 151);
             textBox3.Name = "textBox3";
-            textBox3.Size = new Size(114, 27);
+            textBox3.Size = new Size(100, 23);
             textBox3.TabIndex = 5;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(456, 136);
-            textBox2.Margin = new Padding(3, 4, 3, 4);
+            textBox2.Location = new Point(399, 102);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(114, 27);
+            textBox2.Size = new Size(100, 23);
             textBox2.TabIndex = 4;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(456, 68);
-            textBox1.Margin = new Padding(3, 4, 3, 4);
+            textBox1.Location = new Point(399, 51);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(114, 27);
+            textBox1.Size = new Size(100, 23);
             textBox1.TabIndex = 3;
             // 
             // button1
             // 
-            button1.Location = new Point(603, 332);
-            button1.Margin = new Padding(3, 4, 3, 4);
+            button1.Location = new Point(528, 249);
             button1.Name = "button1";
-            button1.Size = new Size(137, 28);
+            button1.Size = new Size(120, 21);
             button1.TabIndex = 1;
             button1.Text = "Показать график";
             button1.UseVisualStyleBackColor = true;
@@ -295,83 +362,30 @@
             // 
             // tabPageHouses
             // 
-            tabPageHouses.Location = new Point(4, 29);
+            tabPageHouses.Location = new Point(4, 24);
+            tabPageHouses.Margin = new Padding(3, 2, 3, 2);
             tabPageHouses.Name = "tabPageHouses";
-            tabPageHouses.Size = new Size(877, 409);
+            tabPageHouses.Size = new Size(766, 304);
             tabPageHouses.TabIndex = 2;
             tabPageHouses.Text = "Жилье";
             tabPageHouses.UseVisualStyleBackColor = true;
             // 
-            // labelAvPulse
+            // tabPageCrime
             // 
-            labelAvPulse.AutoSize = true;
-            labelAvPulse.Location = new Point(487, 12);
-            labelAvPulse.Name = "labelAvPulse";
-            labelAvPulse.Size = new Size(113, 20);
-            labelAvPulse.TabIndex = 6;
-            labelAvPulse.Text = "Средний пульс";
-            // 
-            // labelMaSpeed
-            // 
-            labelMaSpeed.AutoSize = true;
-            labelMaSpeed.Location = new Point(692, 12);
-            labelMaSpeed.Name = "labelMaSpeed";
-            labelMaSpeed.Size = new Size(179, 20);
-            labelMaSpeed.TabIndex = 6;
-            labelMaSpeed.Text = "Максимальная скорость";
-            // 
-            // labelMiSpeed
-            // 
-            labelMiSpeed.AutoSize = true;
-            labelMiSpeed.Location = new Point(487, 100);
-            labelMiSpeed.Name = "labelMiSpeed";
-            labelMiSpeed.Size = new Size(175, 20);
-            labelMiSpeed.TabIndex = 6;
-            labelMiSpeed.Text = "Минимальная скорость";
-            // 
-            // labelAvSpeed
-            // 
-            labelAvSpeed.AutoSize = true;
-            labelAvSpeed.Location = new Point(737, 100);
-            labelAvSpeed.Name = "labelAvSpeed";
-            labelAvSpeed.Size = new Size(134, 20);
-            labelAvSpeed.TabIndex = 6;
-            labelAvSpeed.Text = "Средняя скорость";
-            // 
-            // labelAvDur
-            // 
-            labelAvDur.AutoSize = true;
-            labelAvDur.Location = new Point(673, 238);
-            labelAvDur.Name = "labelAvDur";
-            labelAvDur.Size = new Size(170, 20);
-            labelAvDur.TabIndex = 6;
-            labelAvDur.Text = "Средняя продолжит-ть";
-            // 
-            // labelTotDur
-            // 
-            labelTotDur.AutoSize = true;
-            labelTotDur.Location = new Point(526, 284);
-            labelTotDur.Name = "labelTotDur";
-            labelTotDur.Size = new Size(159, 20);
-            labelTotDur.TabIndex = 6;
-            labelTotDur.Text = "Общая продолжит-ть";
-            // 
-            // labelDist
-            // 
-            labelDist.AutoSize = true;
-            labelDist.Location = new Point(673, 326);
-            labelDist.Name = "labelDist";
-            labelDist.Size = new Size(113, 20);
-            labelDist.TabIndex = 6;
-            labelDist.Text = "Пройдено (км)";
+            tabPageCrime.Location = new Point(4, 24);
+            tabPageCrime.Name = "tabPageCrime";
+            tabPageCrime.Padding = new Padding(3);
+            tabPageCrime.Size = new Size(766, 304);
+            tabPageCrime.TabIndex = 3;
+            tabPageCrime.Text = " Преступность";
+            tabPageCrime.UseVisualStyleBackColor = true;
             // 
             // View
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(899, 455);
+            ClientSize = new Size(787, 341);
             Controls.Add(tabControl1);
-            Margin = new Padding(3, 4, 3, 4);
             Name = "View";
             Text = "App";
             Load += View_Load;
@@ -418,5 +432,6 @@
         private Label labelMiSpeed;
         private Label labelMaSpeed;
         private Label labelAvPulse;
+        private TabPage tabPageCrime;
     }
 }
